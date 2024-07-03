bf45ab17c300c5bfefae3b6d91b901891dc286bc
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2024-07-03 21:04:09 +0900

    feat(endpoint): implement star wallpaper endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,46 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+export interface Env {
+	// If you set another name in wrangler.toml as the value for 'binding',
+	// replace "AI" with the variable name you defined.
+	AI: Ai;
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env): Promise<Response> {
+		// base keywords
+		const randomKeywords = [
+			'stars',
+			'galaxies',
+			'nebulae',
+			'cosmic dust',
+			'deep space',
+			'vibrant colors',
+			'high resolution',
+			'distant planets',
+			'glowing effects',
+			'shooting star',
+			'milky way',
+			'dark background',
+			'3D rendering',
+			'ethereal atmosphere',
+		];
+		const promptWords = ['space wallpaper', 'anime'];
+		for (let i = 0; i < 10; i++) {
+			promptWords.push(randomKeywords[Math.floor(Math.random() * randomKeywords.length)]);
+		}
+
+		// add random string
+		const randomString = Math.random().toString(36).slice(-8);
+		promptWords.push(randomString);
+
+		// generate image
+		const response = await env.AI.run('@cf/lykon/dreamshaper-8-lcm', {
+			prompt: promptWords.join(', '),
+		});
+
+		return new Response(response, {
+			headers: {
+				'content-type': 'image/png',
+			},
+		});
 	},
 } satisfies ExportedHandler<Env>;
